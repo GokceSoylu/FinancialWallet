4fdcedb3ce68af4e72742b00a3b0a55e1253d6c2
GokceSoylu <gokcesoylu24@gmail.com>
2026-08-12 08:49:53 +0300

    model user added

---
+++ FinancialWallet.Api/Entities/User.cs
@@ -0,0 +1,11 @@
+namespace FinancialWallet.Api.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    //navigation properties
+    public ICollection<Wallet> Wallets { get; set; } = new List<Wallet>();
+}
